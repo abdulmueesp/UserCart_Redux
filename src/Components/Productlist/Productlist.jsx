1e198e822e19626adebf23a403ce7d9a4d330059
abdulmueesp <abdulmueesp5@gmail.com>
2024-05-23 16:08:15 +0530

    useSelector hook used fetch the state

--- src/Components/Productlist/Productlist.jsx
+++ src/Components/Productlist/Productlist.jsx
@@ -3,7 +3,14 @@ import products from "../../api/products.json"
 import "../Productlist/Productlist.css"
 import AfterCart from '../Cartbuttons/AfterCart'
 import BeforeCart from '../Cartbuttons/BeforeCart'
+import { useSelector } from 'react-redux'
+
+
 const Productlist = () => {
+  
+  const {cartCount}=useSelector((state)=>state.cart)
+  console.log(`use selcetor redux ${cartCount}`);
+
   
   const [count,setCount]=useState(0);
   const addtocart=()=>{
