acc7ee0c5d9e459c157fa53eca52706e568ef8ca
abdulmueesp <abdulmueesp5@gmail.com>
2024-05-23 16:42:38 +0530

    UseDispatch use function is working in redux store

--- src/Components/Cartbuttons/BeforeCart.jsx
+++ src/Components/Cartbuttons/BeforeCart.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import "./Cartbutons.css"
-const BeforeCart = ({addtocart}) => {
+import { useDispatch } from 'react-redux'
+import { addtocart } from '../../Redux/Cart'
+
+
+const BeforeCart = () => {
+
+  const dispatch=useDispatch();
+
   return (
+
    <div className="before-cart">
-    <button className='add-cart-button' onClick={addtocart}>Add to cart</button>
+    <button className='add-cart-button' onClick={()=>dispatch(addtocart())} >
+      Add to cart
+      </button>
    </div>
+   
   )
 }
+
+
 
 export default BeforeCart
--- src/Components/Productlist/Productlist.jsx
+++ src/Components/Productlist/Productlist.jsx
@@ -9,21 +9,15 @@ import { useSelector } from 'react-redux'
 const Productlist = () => {
   
   const {cartCount}=useSelector((state)=>state.cart)
-  console.log(`use selcetor redux ${cartCount}`);
-
   
-  const [count,setCount]=useState(0);
-  const addtocart=()=>{
-     setCount(1);
-  }
-    
+
   return (
    <section className='container'>
      {products?.map((data)=>(
       <div className="product-container" key={data.id}>
         <img src={data?.image} alt=""  />
         <h3>{data?.title}</h3>
-        {count>0 ?<AfterCart />:<BeforeCart  addtocart={addtocart} />}
+        {cartCount>0 ?<AfterCart />:<BeforeCart />}
         
         
       </div>
